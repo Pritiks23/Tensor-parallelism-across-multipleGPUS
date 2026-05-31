cca9cb274d2d5e60952d453588769099e9153810
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-05-31 14:18:40 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
+import torch.nn as nn
+from tp_layers import TPLinear, TPAttention
 
+class TinyTPTransformer(nn.Module):
+    def __init__(self, dim, heads, tp_size, rank):
+        super().__init__()
+
+        self.attn = TPAttention(dim, heads, tp_size, rank)
+        self.mlp = TPLinear(dim, 4 * dim, tp_size, rank)
+        self.proj = nn.Linear(4 * dim, dim)
+
+    def forward(self, x):
+        x = x + self.attn(x)
+        x = x + self.mlp(x)
+        return self.proj(x)
