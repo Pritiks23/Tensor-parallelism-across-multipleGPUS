7ae6c07940550eb146d04d3e314bf05ca54fb049
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-05-31 14:18:10 -0700

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,1 +1,16 @@
+import time
+import torch
+import torch.distributed as dist
 
+def benchmark(model, x, steps=100):
+    torch.cuda.synchronize()
+    start = time.time()
+
+    for _ in range(steps):
+        model(x)
+
+    torch.cuda.synchronize()
+    end = time.time()
+
+    tps = steps / (end - start)
+    return tps
